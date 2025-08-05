d19e3a1bd65846600ec2222af1d31a78107fb676
Dusan <dusan.daniska@wai.sk>
2025-08-05 08:40:39 +0200

    build from local repositories

--- bin/build-from-local-repositories.bat
+++ bin/build-from-local-repositories.bat
@@ -1,8 +1,8 @@
 cd %~dp0..
-copy %~dp0../composer-local-repositories.json %~dp0../composer.json
-copy %~dp0../package-local-repositories.json %~dp0../package.json
+copy %~dp0..\composer-local-repositories.json %~dp0..\composer.json
+copy %~dp0..\package-local-repositories.json %~dp0..\package.json
 
-composer update
-npm install
-npm run build
-php hubleto init %~dp0../init-config-example.yaml
+call composer update
+call npm install
+call npm run build
+call php hubleto init %~dp0..\init-config-example.yaml
